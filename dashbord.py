a41eac46f8e003200240be00f9c5b9def726b23b
Sebastian Weiß <sw@3ele.de>
2022-10-04 18:08:57 +0200

    add template

--- dashbord.py
+++ dashbord.py
@@ -6,9 +6,22 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         with open('data.json', 'r') as f:
             data = json.load(f)
-            print (data)
-
-        self.render("template.html", title="My title", items=data[0])
+         #   print (data)
+            mylist =[]
+            
+            for site in data:
+                for key, value in site.items():
+                    
+                    domain = {
+                        'domain' : key,
+                        'values' : value
+                    }
+                    domain['alert'] = False  
+                    if len(value['messages']['errors']) > 0:
+                        domain['alert'] = True    
+                    mylist.append(domain)
+                    
+        self.render("template.html", title="Downloads", items=mylist)
 
 def make_app():
     return tornado.web.Application([
@@ -17,7 +30,7 @@ def make_app():
 
 async def main():
     app = make_app()
-    app.listen(8888)
+    app.listen(8889)
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
